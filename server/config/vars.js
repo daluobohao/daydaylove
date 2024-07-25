ea77016a026e4620f41deeff379f9f633151e54a
liujinkai <ljinkai@126.com>
2024-07-25 22:00:07 +0800

    new

--- server/config/vars.js
+++ server/config/vars.js
@@ -1,17 +1,17 @@
 // yungouos开发配置
 const YUNGOU = {
-    mch_id: "1635138365", // 商户ID
-    YUNGOUOS_KEY: "555996BFBDB54D0397663F42B8511FE0", // yungouos开发key
-    notify_url: "https://52e31w892.imdo.co/api/pay/yungou", // 回调通知地址
+    mch_id: "xx", // 商户ID
+    YUNGOUOS_KEY: "xx", // yungouos开发key
+    notify_url: "xx", // 回调通知地址
     api_url: "https://api.pay.yungouos.com/api/pay/wxpay/nativePay",
     default_name: "DayDayMoment会员"
 }
 
 // Leancloud开发配置
 const AV_CONFIG = {
-    appId: "aiqWSf9U11Yyu7d1e7xzogjD-gzGzoHsz",
-    appKey: "nRIwI35Qj0OYmPuXZ6Cj25gI",
-    masterKey: "VxRpHZ6ToyHyeBgynXcf3Vny",
-    serverURL: "https://aiqwsf9u.lc-cn-n1-shared.com"
+    appId: "xxx",
+    appKey: "xxx",
+    masterKey: "xxx",
+    serverURL: "xxx"
 }
 export { AV_CONFIG, YUNGOU }
