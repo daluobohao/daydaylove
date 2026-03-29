93021b3bfd8ccff65df39a6fd93aa9c547c5d299
daluobohao <1103095152@qq.com>
2026-03-29 20:10:06 +0800

    feat: modify url
    Signed-off-by: daluobohao <1103095152@qq.com>

--- server/config/vars.js
+++ server/config/vars.js
@@ -3,7 +3,8 @@ const YUNGOU = {
     mch_id: "1105317738", // 商户ID
     YUNGOUOS_KEY: "627B69BDB3E54120926F49604C01832B", // yungouos开发key
     // notify_url: "https://wang-hao-hao.cn/api/pay/yungou", // 回调通知地址
-    notify_url: "https://howard-foster-affect-rock.trycloudflare.com/api/pay/yungou",
+    // notify_url: "https://howard-foster-affect-rock.trycloudflare.com/api/pay/yungou",
+    notify_url: "https://aroundfe.info/api/pay/yungou",
     api_url: "https://api.pay.yungouos.com/api/pay/wxpay/nativePay",
     default_name: "daydaylove会员"
 }
